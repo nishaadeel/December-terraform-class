a0719c349aade09cbc6a27922e6b4add48d94d2f
Your Name <you@example.com>
2023-03-16 00:23:48 +0000

    added aws provider

---
+++ Class1/provider.tf
@@ -0,0 +1,16 @@
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+
+# # Configure the Microsoft Azure Provider
+# provider "azurerm" {
+#   features {}
+# }
+
+
+# provider "google" {
+#   project     = "my-project-id"
+#   region      = "us-central1"
+# }
