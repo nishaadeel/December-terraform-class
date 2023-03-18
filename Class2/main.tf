7c0b5b103ab155cd148aa8108df01b6704e2a336
Your Name <you@example.com>
2023-03-18 15:20:03 +0000

    added az

--- Class2/main.tf
+++ Class2/main.tf
@@ -2,4 +2,9 @@
 resource "aws_instance" "web" {
   ami           = "ami-02f3f602d23f1659d"
   instance_type = "t3.micro"
+  associate_public_ip_address  = true
+  availability_zone = "us-east-1a"
 }
+
+
+
