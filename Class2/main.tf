2de3a485c212d1265a494cf73ec8b1c67a437384
Your Name <you@example.com>
2023-03-18 15:32:21 +0000

    added sec group

--- Class2/main.tf
+++ Class2/main.tf
@@ -1,3 +1,10 @@
+# creates a key pair
+resource "aws_key_pair" "class2" {
+  key_name   = "class2-key"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+
 # creates ec2 instance
 resource "aws_instance" "web" {
   ami           = "ami-02f3f602d23f1659d"
@@ -8,8 +15,33 @@ resource "aws_instance" "web" {
 }
 
 
-# creates a key pair
-resource "aws_key_pair" "class2" {
-  key_name   = "class2-key"
-  public_key = file("~/.ssh/id_rsa.pub")
+
+# creates sec group
+resource "aws_security_group" "class2" {
+  name        = "class2"
+  description = "Allow TLS inbound traffic"
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1" 
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 }
