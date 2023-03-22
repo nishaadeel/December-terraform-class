92c7cb4ad24d2b3deb8f7d8517fded3e235e7640
farrukh90 <farrukhsadykov@gmail.com>
2023-03-22 23:36:10 +0000

    added project

--- Class4/account/main.tf
+++ Class4/account/main.tf
@@ -7,3 +7,19 @@ data "google_billing_account" "acct" {
 output  billing_account_name {
     value = data.google_billing_account.acct.name
 }
+
+
+resource "random_password" "password" {
+	length = 16
+	numeric = false
+	special = false
+	lower = true
+	upper = false
+}
+
+
+resource "google_project" "testproject" {
+	name = "testproject"
+	project_id = random_password.password.result
+	billing_account = data.google_billing_account.acct.id
+}
