82ad2b517a9b5c1d59902beced49a3eb209d036c
Your Name <you@example.com>
2023-03-25 16:35:47 +0000

    added variable

--- Class5/main.tf
+++ Class5/main.tf
@@ -1,16 +1,15 @@
 resource "aws_key_pair" "state-demo" {
   key_name   = "state-demo-key"
   public_key = file("~/.ssh/id_rsa.pub")
-  tags = {
-    Name = "Demo"
-  }
+  tags = var.tags
 }
 
 resource "aws_key_pair" "state-demo2" {
   key_name   = "state-demo2-key"
   public_key = file("~/.ssh/id_rsa.pub")
-  tags = {
-    Name = "Demo"
-    Team = "DevOps"
-  }
+  tags = var.tags
 }
+
+
+
+
---
+++ Class5/variable.tf
@@ -0,0 +1,13 @@
+variable tags {
+  type = map
+  default = {
+    Name = "Demo"
+    Team = "DevOps" 
+    Quarter = 2
+  }
+}
+
+
+variable region {
+  default = "us-east-2"
+}
