68bd75a79ec09b60761f63fe9fece60f00f9acfc
Your Name <you@example.com>
2023-03-25 18:17:21 +0000

    added description

--- Class5/variable.tf
+++ Class5/variable.tf
@@ -1,14 +1,26 @@
 variable "tags" {
+  description = "Please provide a map of tags for each resource"
   type = map(any)
+  default = {
+    Env = "dev"
+  }
 }
 
-variable "region" {}
+variable "region" {
+  description = "Please provide a region"
+}
 
-variable "key_name1" {}
+variable "key_name1" {
+  description = "Please provide a key name for first one"
+}
 
-variable "key_name2" {}
+variable "key_name2" {
+  description = "Please provide a key name for second one"
+}
 
-variable "public_key" {}
+variable "public_key" {
+  description = "Please provide a key location"
+}
 
 variable vpc_cidr_block {}
 variable public_subnet_1_cidr_block {}
