a0ce748c6c4a154c245cd4adadf4f6a79111f711
Your Name <you@example.com>
2023-03-25 15:41:21 +0000

    added 2 key pairs

--- Class5/main.tf
+++ Class5/main.tf
@@ -2,3 +2,8 @@ resource "aws_key_pair" "state-demo" {
   key_name   = "state-demo-key"
   public_key = file("~/.ssh/id_rsa.pub")
 }
+
+resource "aws_key_pair" "state-demo2" {
+  key_name   = "state-demo2-key"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
