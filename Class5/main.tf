bded403c27248be939267bc04aa49777ca3dcfa0
Your Name <you@example.com>
2023-03-25 17:47:52 +0000

    added sec group

--- Class5/main.tf
+++ Class5/main.tf
@@ -9,3 +9,27 @@ resource "aws_key_pair" "state-demo2" {
   public_key = file(var.public_key)
   tags       = var.tags
 }
+
+resource "aws_security_group" "important" {
+  name        = "important"
+  description = "Allow TLS inbound traffic"
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
