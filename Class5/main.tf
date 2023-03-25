1be43cc54c3068a92d0ae479c8258178e2f2ec1f
Your Name <you@example.com>
2023-03-25 18:10:05 +0000

    added more variables

--- Class5/main.tf
+++ Class5/main.tf
@@ -36,29 +36,26 @@ resource "aws_security_group" "important" {
 }
 
 resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block = var.vpc_cidr_block
   tags       = var.tags
 }
+
 
 resource "aws_subnet" "subnet1" {
   availability_zone = data.aws_availability_zones.available.names[0]
   vpc_id  = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
+  cidr_block = var.public_subnet_1_cidr_block
 }
 
 
 resource "aws_subnet" "subnet2" {
   availability_zone = data.aws_availability_zones.available.names[1]
   vpc_id  = aws_vpc.main.id
-  cidr_block = "10.0.2.0/24"
+  cidr_block = var.public_subnet_2_cidr_block
 }
 
 resource "aws_subnet" "subnet3" {
   availability_zone = data.aws_availability_zones.available.names[2]
   vpc_id  = aws_vpc.main.id
-  cidr_block = "10.0.3.0/24"
-}
-
-output azs {
-  value = data.aws_availability_zones.available.names
+  cidr_block = var.public_subnet_3_cidr_block
 }
--- Class5/variable.tf
+++ Class5/variable.tf
@@ -9,3 +9,8 @@ variable "key_name1" {}
 variable "key_name2" {}
 
 variable "public_key" {}
+
+variable vpc_cidr_block {}
+variable public_subnet_1_cidr_block {}
+variable public_subnet_2_cidr_block {}
+variable public_subnet_3_cidr_block {}
