c76cdea9e208b66174ccbeae65c80f52e3abe2ca
Your Name <you@example.com>
2023-03-25 18:01:46 +0000

    added az

--- Class5/main.tf
+++ Class5/main.tf
@@ -39,3 +39,22 @@ resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
   tags       = var.tags
 }
+
+resource "aws_subnet" "subnet1" {
+  availability_zone = data.aws_availability_zones.available.names[0]
+  vpc_id  = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+}
+
+
+resource "aws_subnet" "subnet2" {
+  availability_zone = data.aws_availability_zones.available.names[1]
+  vpc_id  = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+}
+
+resource "aws_subnet" "subnet3" {
+  availability_zone = data.aws_availability_zones.available.names[2]
+  vpc_id  = aws_vpc.main.id
+  cidr_block = "10.0.3.0/24"
+}
