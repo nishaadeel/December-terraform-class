1d9f554f11e2b7d225d0c2ec77f3358fb6f3d05b
Your Name <you@example.com>
2023-03-29 23:18:15 +0000

    added tags

--- Class7/main.tf
+++ Class7/main.tf
@@ -1,4 +1,3 @@
-
 # Read all available AZ in ohio
 data "aws_availability_zones" "all" {}
 
@@ -10,24 +9,24 @@ output "AZ" {
 
 
 
-# resource "aws_default_subnet" "default_az1" {
-# 	availability_zone = data.aws_availability_zones.all.names[0]
-# 	tags = {
-# 		Name = "Subnet1"
-# 	}
-# }
-# resource "aws_default_subnet" "default_az2" {
-# 	availability_zone = data.aws_availability_zones.all.names[1]
-# 	tags = {
-# 		Name = "Subnet2"
-# 	}
-# }
-# resource "aws_default_subnet" "default_az3" {
-# 	availability_zone = data.aws_availability_zones.all.names[2]
-# 	tags = {
-# 		Name = "Subnet3"
-# 	}
-# }
+resource "aws_default_subnet" "default_az1" {
+	availability_zone = data.aws_availability_zones.all.names[0]
+	tags = {
+		Name = "Subnet1"
+	}
+}
+resource "aws_default_subnet" "default_az2" {
+	availability_zone = data.aws_availability_zones.all.names[1]
+	tags = {
+		Name = "Subnet2"
+	}
+}
+resource "aws_default_subnet" "default_az3" {
+	availability_zone = data.aws_availability_zones.all.names[2]
+	tags = {
+		Name = "Subnet3"
+	}
+}
 
 # output "subnet1" {
 # 	value = [ 
