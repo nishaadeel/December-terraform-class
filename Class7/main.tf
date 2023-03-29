87ad50f41bd969929287cbb387f0dbd76e615fdb
Your Name <you@example.com>
2023-03-29 23:29:45 +0000

    added subnet group

--- Class7/main.tf
+++ Class7/main.tf
@@ -70,4 +70,5 @@ resource "aws_db_instance" "default" {
   parameter_group_name = "default.mysql5.7"
   skip_final_snapshot  = true
   vpc_security_group_ids = [aws_security_group.allow_mysql.id]
+  db_subnet_group_name  = aws_db_subnet_group.default.name
 }
