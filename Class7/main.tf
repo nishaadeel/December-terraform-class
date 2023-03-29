77af487dc2932d94451e273c750e5bb2db979978
Your Name <you@example.com>
2023-03-29 23:24:42 +0000

    added sec group

--- Class7/main.tf
+++ Class7/main.tf
@@ -37,3 +37,24 @@ resource "aws_db_subnet_group" "default" {
     Name = "My DB subnet group"
   }
 }
+
+
+# Create a sec group
+resource "aws_security_group" "allow_mysql" {
+  name        = "allow_mysql"
+  description = "Allow TLS inbound traffic"
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 3306
+    to_port          = 3306
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
