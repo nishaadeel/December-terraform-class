fe533b7b54931d101225ebc4f71a73af9850e4f0
Your Name <you@example.com>
2023-04-01 15:44:54 +0000

    added cidr

--- Team2/main.tf
+++ Team2/main.tf
@@ -36,8 +36,7 @@ resource "aws_security_group" "datasource" {
     from_port        = 443
     to_port          = 443
     protocol         = "tcp"
-    cidr_blocks      = [aws_vpc.main.cidr_block]
-    ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+    cidr_blocks      = ["0.0.0.0/0"]
   }
 
   egress {
