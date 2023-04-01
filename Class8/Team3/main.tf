e15aece9440902eb51100f5864a0c714cf12ad80
Your Name <you@example.com>
2023-04-01 16:11:28 +0000

    reorganized folders

---
+++ Class8/Team3/main.tf
@@ -0,0 +1,53 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+  subnet_id   = data.terraform_remote_state.main.outputs.public_subnets[0]
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+
+
+resource "aws_security_group" "datasource" {
+  name        = "datasource"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = data.terraform_remote_state.main.outputs.vpc_id
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
