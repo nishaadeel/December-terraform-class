163bf7b5fbf9920f74e8eded34bfce0a46e2ad96
Your Name <you@example.com>
2023-04-01 15:44:37 +0000

    added sec group

--- Team2/main.tf
+++ Team2/main.tf
@@ -23,3 +23,32 @@ resource "aws_instance" "web" {
     Name = "HelloWorld"
   }
 }
+
+
+
+resource "aws_security_group" "datasource" {
+  name        = "datasource"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = data.terraform_remote_state.main.outputs.vpc_id
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.main.cidr_block]
+    ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
